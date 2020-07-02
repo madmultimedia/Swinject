eecded6b2b70b6f5692868d372fa7a5584f61109
Rick Pasveer <rickpasveer@gmail.com>
2020-07-02 09:10:21 +0200

    Update Package.swift for swift 5.3

--- Package.swift
+++ Package.swift
@@ -4,6 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "Swinject",
+      platforms: [
+        .macOS(.v10_10),
+        .iOS(.v9),
+        .tvOS(.v9),
+    ],
     products: [
         .library(name: "Swinject", targets: ["Swinject"]),
     ],
